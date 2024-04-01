8382c6c4bf23ed15952b284dd9b442d3483ea745
Sanay Nesargi <sanaynesargi@gmail.com>
2024-04-01 18:25:38 -0500

    t

--- test.js
+++ test.js
@@ -3,24 +3,24 @@ const adBlockers = [
     site: "https://greenhill.myschoolapp.com/app/student#studentmyday/progress",
     adText: "Conduct",
     addElementSelector:
-      "#conduct > div.bb-tile-title > div.bb-tile-header-with-content > h2",
+      "#site-main > div > div > div > div.well.well-sm > label",
   },
 ];
 
-const host = document.location.host.replace("www.", "");
+// const host = document.location.host.replace("www.", "");
 
-const adBlocks = adBlockers.filter((adBlock) => {
-  var site = adBlock.site.split(",");
-  return site.includes("green");
-});
+// const adBlocks = adBlockers.filter((adBlock) => {
+//   var site = adBlock.site.split(",");
+//   return site.includes("green");
+// });
 
 adBlocks.forEach((adBlock) => {
-  var adText = adBlock.adText.split(",");
-  var adTextContainer = adBlock.adTextContainer || "span";
   var adElementSelector = adBlock.adElementSelector;
 
   setInterval(function () {
     const t = document.querySelector(adElementSelector);
+
+    t.innerText = "AHAHAH";
 
     alert("very good!");
   }, 1000);
